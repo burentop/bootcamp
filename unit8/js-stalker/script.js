bf25a71f5882c1c037e15a5fda4b0d7662fdfbc3
Brent Perry <burentoperii@gmail.com>
2017-04-25 14:03:55 -0500

    add JS Stalker exercise

---
+++ unit8/js-stalker/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt(firstName + ", what is your last name?");
+var age = prompt(firstName + ", how old are you?");
+var fullName = firstName + " " + lastName;
+console.log("Your full name is " + fullName);
+console.log("You are " + age + " years old");
